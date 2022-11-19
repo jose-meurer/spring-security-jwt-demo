499822724edcd5c21563351eea44a6c92bf18981
josemeurer <josewmeurer@gmail.com>
2022-11-19 20:30:11 -0300

    FEAT Security jwt

--- src/main/java/dev/josemeurer/jwt/JwtApplication.java
+++ src/main/java/dev/josemeurer/jwt/JwtApplication.java
@@ -1,9 +1,12 @@
 package dev.josemeurer.jwt;
 
+import dev.josemeurer.jwt.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class JwtApplication {
 
 	public static void main(String[] args) {
